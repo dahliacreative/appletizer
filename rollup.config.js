ac25ab96efed5e35c35e12702ae700567a17c6cc
dahliacreative <simon@dahliacreative.com>
2022-10-26 09:41:38 +0100

    Removing source maps, updating to reuse root

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import { eslint } from 'rollup-plugin-eslint';
+import { eslint } from "rollup-plugin-eslint";
 
 const packageJson = require("./package.json");
 
@@ -13,12 +13,12 @@ export default [
       {
         file: packageJson.main,
         format: "cjs",
-        sourcemap: true,
+        sourcemap: false,
       },
       {
         file: packageJson.module,
         format: "esm",
-        sourcemap: true,
+        sourcemap: false,
       },
     ],
     plugins: [
--- src/methods/initializeApplet.tsx
+++ src/methods/initializeApplet.tsx
@@ -7,15 +7,18 @@ export const initializeApplet = (
   Applet: React.FC,
   developmentContext: any = {}
 ) => {
+  let root: ReactDom.Root | null = null;
   (window as any)[`mount${name}`] = (containerId: string, context: any) => {
-    const root = ReactDom.createRoot(document.getElementById(containerId)!);
+    if (!root) {
+      root = ReactDom.createRoot(document.getElementById(containerId)!);
+    }
     root.render(
       <AppletContextProvider context={context}>
         <Applet />
       </AppletContextProvider>
     );
     (window as any)[`unmount${name}`] = () => {
-      root.unmount();
+      root!.unmount();
     };
   };
 
